a110c2b8ec0f2d533eaf31f1ee87f5c022cebe99
Yoon Hong <yoon.s.hong@gmail.com>
2012-09-16 13:43:18 +0800

    PhoneGap Setting

--- src/net/yoonh/masskara/MainActivity.java
+++ src/net/yoonh/masskara/MainActivity.java
@@ -1,20 +1,14 @@
 package net.yoonh.masskara;
 
+import org.apache.cordova.DroidGap;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends DroidGap {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+                
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
